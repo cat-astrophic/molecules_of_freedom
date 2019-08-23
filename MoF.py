96a7362cee192599ba5859140392308625d78b44
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2019-08-23 13:32:35 -0400

    updated to include choropleths

--- MoF.py
+++ MoF.py
@@ -1,20 +1,24 @@
-# This script performs the statistical analysis for Molecules of Freedom
-
-# For clarity, 'Molecules of Freedom' is an idiotic catchphrase used by the 45th administration to reference natural gas
+# This script performs the statistical analysis for Molecules of Freedom'
 
 # Importing required modules
 
 import pandas as pd
 import statsmodels.api as stats
 import pysal
+import geopandas as gpd
+import matplotlib.pyplot as plt
 
 # Loading the data ses and storing it in a dataframe
 
 spatial_filepath = 'C:/Users/User/Documents/Data/MOF.csv'
 sw_filepath = 'C:/Users/User/Documents/Data/MOF_W.csv'
+mapdata_filepath = 'C:/Users/User/Documents/Data/MoFmAp.csv'
+shpath = 'C:/Users/User/Documents/Data/countries.shp'
 
 spdata = pd.read_csv(spatial_filepath)
 SW = pd.read_csv(sw_filepath, header = None)
+mapdat = pd.read_csv(mapdata_filepath)
+shpdat = gpd.read_file(shpath)
 
 # Creating dataframes for all regression models
 
@@ -29,7 +33,7 @@ Y = spdata['Intensity']
 Xsp1 = spdata[['Intensity_Lag', 'GDP', 'Renewable Energy', 'Coal Rents', 'Oil Rents', 'Crude Price', 'R&D', 'Tariff Rate']]
 Xsp2 = spdata[['Intensity_Lag', 'GDP', 'Renewable Energy', 'Coal Rents', 'Oil Rents', 'Crude Price', 'R&D', 'Target Tariff']]
 
-# Dataframes ready for baselines OLS regression
+# Dataframes ready for baseline OLS regression
 
 X1 = stats.add_constant(Xsp1)
 X2 = stats.add_constant(Xsp2)
@@ -78,17 +82,30 @@ file = open('C:/Users/User/Documents/Data/MoF/splag2.txt', 'w')
 file.write(sp2.summary)
 file.close()
 
+# Creating choropleths for tariff rates and carbon intensities
 
+# Blending the data and shape files
 
+shpdat = shpdat[['COUNTRY', 'geometry']]
+merged = shpdat.set_index('COUNTRY').join(mapdat.set_index('Country'))
 
+# Creating a reference dictionary for looping choropleth creation and additional map parameters
 
+vals = [str(mapdat.columns[i]) for i in range(1,len(mapdat.columns))]
+titles = ['Carbon Intensities - 2012', 'Carbon Intensities - 1996', 'Mean Tariff Rates - 2012', 'Mean Tariff Rates - 1996']
+dic = dict(zip(vals, titles))
+cols = ['Purples', 'Reds', 'Purples', 'Reds']
 
+# Creating and saving the choropleths
 
-
-
-
-
-
-
-
+for val, tit in dic.items():
+    print('Creating choropleth for ' + tit + '\n')
+    idx = vals.index(val)
+    fig, ax = plt.subplots(1, figsize = (12,6))
+    plt.axis('off')
+    merged.plot(color = 'lightgray', linewidth = 0.5, ax = ax, edgecolor = '0.8', legend = False)
+    plotdata = merged[['geometry', val]].dropna()
+    plotdata.plot(column = val, cmap = cols[idx], linewidth = 0.5, ax = ax, edgecolor = '0.8', legend = True)
+    plt.title(tit, fontsize = 14, fontweight = 40)
+    plt.savefig('C:/Users/User/Documents/Data/MoF/' + val + '.png')
 
